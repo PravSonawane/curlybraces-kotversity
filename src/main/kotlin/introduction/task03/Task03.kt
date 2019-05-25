8dfec34657e2ffa41b7510a94f81664b28b00cb3
PravinSonawane <june.pravin@gmail.com>
2019-05-25 15:35:18 -0700

    Created task03
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

---
+++ src/main/kotlin/introduction/task03/Task03.kt
@@ -0,0 +1,23 @@
+package introduction.task03
+
+/*
+    Tasks:
+        1. Define a function printString() that prints "Kotlin"
+        2. Change the function to take a string parameter and make it print that
+        3. Define a new function add() that takes two params and returns their sum
+        3. Write a unit test for these functions
+ */
+
+/*
+    Aspects:
+        1. Defining function syntax
+            a. Regular style
+            b. Expression body
+            c. Inferred return type
+        2. Function parameters
+            a. Parameter type is after its name
+        3. No primitive types in Kotlin
+*/
+
+fun main() {
+}
