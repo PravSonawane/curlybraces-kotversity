c358ef117d785a7216fc053de94959046192dbd8
PravinSonawane <june.pravin@gmail.com>
2019-05-25 20:01:17 -0700

    Created task04

---
+++ src/main/kotlin/introduction/task04/Task04.kt
@@ -0,0 +1,23 @@
+package introduction.task04
+
+/*
+    Tasks:
+        1. Define a function called wrap() that wraps a String with a prefix and suffix Char
+        2. Write a unit test for this function
+        3. Let's say prefix and suffix are optional and default to [ and ]
+        4. Let's say we need strings as prefixes and suffixes too.
+        5. Define a function that can three arguments and return their sum
+        6. Write a unit test for this function
+        7. Define a function to take any number of arguments and return their sum
+ */
+
+/*
+    Aspects:
+        1. Named arguments
+        2. Default arguments
+        3. Function overloads
+        4. vararg arguments
+*/
+
+fun main() {
+}
